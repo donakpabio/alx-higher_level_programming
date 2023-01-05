6d4597f3959957c26b423596622a1745f745f1fc
Namkere Akpabio <donakpabio@gmail.com>
2023-01-04 19:54:06 -0800

    t

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 islower = __import__('7-islower').islower
+
+
 def uppercase(str):
     alpha1 = "abcdefghijklmnopqrstuvwxyz"
     alpha2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     for n in range(len(str)):
         myend = '\n' if (n == (len(str) - 1)) else ''
         if islower(str[n]):
-            print("{}".format(alpha2[alpha1.rfind(str[n])]), end = myend)
+            print("{}".format(alpha2[alpha1.rfind(str[n])]), end=myend)
         else:
-            print("{}".format(str[n]),  end = myend)
+            print("{}".format(str[n]), end=myend)
