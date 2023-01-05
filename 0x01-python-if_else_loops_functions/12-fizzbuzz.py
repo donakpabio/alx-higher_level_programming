8e247a7a607113a0ce7588ef3df6a2e8b581e3c0
Namkere Akpabio <donakpabio@gmail.com>
2023-01-04 20:20:48 -0800

    t

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for n in range(1, 101):
+        if n % 3 == 0:
+            print("{}".format("Fizz"), end=' ')
+        elif n % 5 == 0:
+            print("{}".format("Buzz"), end=' ')
+        else:
+            print("{}".format(n), end=' ')
