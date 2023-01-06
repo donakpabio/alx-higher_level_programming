4eabec12c5291e3bad0b48fb01bbd87dcb09c6bb
Namkere Akpabio <donakpabio@gmail.com>
2023-01-05 20:22:50 -0800

    t

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys, hidden_4
+
+
+def add1():
+    list = dir(hidden_4)
+    for n in range(1, len(list)):
+        if "__" not in list[n]:
+            print("{}".format(list[n]))
+
+if __name__ == "__main__":
+    add1()
