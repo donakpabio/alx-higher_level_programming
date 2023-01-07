54cec6f32b3cea28fa95600edaf65e12e119dcbb
Namkere Akpabio <donakpabio@gmail.com>
2023-01-07 03:56:22 -0800

    t

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,10 +2,11 @@
 
 
 def no_c(my_string):
-    last = len(my_string) - 1
+    text = ""
     for n in range(len(my_string)):
         if my_string[n] not in "cC":
-            print("{}".format(my_string[n]), end='\n' if n == last else '')
+            text = text + my_string[n]
+    return text
 
 
 if __name__ == "__main__":
