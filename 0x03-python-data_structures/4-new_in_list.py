3e3234c6eaf203698d1b2ceaac4fc4e2557d5a10
Namkere Akpabio <donakpabio@gmail.com>
2023-01-06 21:12:26 -0800

    t

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx > len(my_list):
+        return my_list
+    else:
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return new_list
+
+
+if __name__ == "__main__":
+    new_in_list(my_list, idx, element)
