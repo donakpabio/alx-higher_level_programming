cb5b0953c870f40801ee7ae39cb629518f5d567d
Namkere Akpabio <donakpabio@gmail.com>
2023-01-07 05:01:24 -0800

    t

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for n in range(len(matrix)):
+        for m in range(len(matrix[n])):
+            if len(matrix[n]) - 1 == m:
+                print("{}".format(matrix[n][m]))
+            else:
+                print("{}".format(matrix[n][m]), end=' ')
+
+    if len(matrix) == 0:
+        print("\n")
+
+if __name__ == "__main__":
+    print_matrix_integer(matrix=[[]])
