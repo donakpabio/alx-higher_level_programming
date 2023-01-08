1d6b75e14b08157ab1a3129f22ed5cbd970d4cef
Namkere Akpabio <donakpabio@gmail.com>
2023-01-08 11:28:57 -0800

    t

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,10 +2,12 @@
 
 
 def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
     max = 0
     for n in range(len(my_list)):
         max = my_list[n] if my_list[n] > max else max
-    return None if len(my_list) == 0 else max
+    return max
 
 if __name__ == "__main__":
     max_integer(my_list=[])
