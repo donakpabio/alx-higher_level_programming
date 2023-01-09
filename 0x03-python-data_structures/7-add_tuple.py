fc4468b088057b4a876f567579f643580dd6fefa
Namkere Akpabio <donakpabio@gmail.com>
2023-01-08 19:04:48 -0800

    t

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,9 +4,9 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     l1 = [0, 0]
     l2 = [0, 0]
-    for n in range(len(tuple_a)):
+    for n in range(min(len(tuple_a), 2)):
         l1[n] = int(tuple_a[n])
-    for n in range(len(tuple_b)):
+    for n in range(min(len(tuple_b), 2)):
         l2[n] = int(tuple_b[n])
 
     return tuple((l1[0] + l2[0], l1[1] + l2[1]))
