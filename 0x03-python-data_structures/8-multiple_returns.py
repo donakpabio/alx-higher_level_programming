c0983c3d39be37931e0440d6afbb7d2254352ca6
Namkere Akpabio <donakpabio@gmail.com>
2023-01-08 19:15:37 -0800

    t

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,8 @@
 
 
 def multiple_returns(sentence):
-    l = len(sentence)
-    return tuple((l, None if l == 0 else sentence[0]))
+    size = len(sentence)
+    return tuple((size, None if size == 0 else sentence[0]))
 
 
 if __name__ == "__main__":
