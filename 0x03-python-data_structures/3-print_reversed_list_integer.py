22040230c4c4c6c693d0daf250cc2d1d092805ea
Namkere Akpabio <donakpabio@gmail.com>
2023-01-09 17:32:45 -0800

    t

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,7 +4,7 @@
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     for n in range(len(my_list)):
-        print("{:d}".format(int(my_list[n])))
+        print("{:d}".format(my_list[n]))
 
 
 if __name__ == "__main__":
