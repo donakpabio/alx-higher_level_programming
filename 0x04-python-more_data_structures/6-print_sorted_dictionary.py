a7d7419d71558a53f3a250ca580d1456a897667b
Namkere Akpabio <donakpabio@gmail.com>
2023-01-11 12:03:27 -0800

    t

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,9 +2,10 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-    sorted(a_dictionary)
-    for x, y in a_dictionary.items():
-        print("{}: {}".format(x, y))
+    dlist = list(a_dictionary)
+    dlist.sort()
+    for n in range(len(dlist)):
+        print("{}: {}".format(dlist[n], a_dictionary[dlist[n]]))
 
 
 if __name__ == "__main__":
