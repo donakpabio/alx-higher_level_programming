0e099eeab39c410e140d439c14cd400f4afa52c2
Namkere Akpabio <donakpabio@gmail.com>
2023-01-11 12:06:58 -0800

    t

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,10 +2,11 @@
 
 
 def print_sorted_dictionary(a_dictionary):
-    dlist = list(a_dictionary)
-    dlist.sort()
-    for n in range(len(dlist)):
-        print("{}: {}".format(dlist[n], a_dictionary[dlist[n]]))
+    if a_dictionary is not None:
+        dlist = list(a_dictionary)
+        dlist.sort()
+        for n in range(len(dlist)):
+            print("{}: {}".format(dlist[n], a_dictionary[dlist[n]]))
 
 
 if __name__ == "__main__":
