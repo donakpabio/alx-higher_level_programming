76c767baa64bf2330c9987acf1b74b87339527da
Namkere Akpabio <donakpabio@gmail.com>
2023-01-12 01:33:06 -0800

    t

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+    rem = []
+    for k, v in a_dictionary.items():
+        if v == value:
+            rem.append(k)
+    
+    for n in range(len(rem)):
+        del a_dictionary[rem[n]]
+    return a_dictionary
+
+
+if __name__ == "__main__":
+    complex_delete(a_dictionary, value)
