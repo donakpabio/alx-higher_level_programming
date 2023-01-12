dc439dd1ebdb566ca93addf3647e2e6cce61024c
Namkere Akpabio <donakpabio@gmail.com>
2023-01-11 19:41:28 -0800

    t

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def multiply_by_2(a_dictionary):
+    new_dic = {}
+    for k, v in a_dictionary.items():
+        new_dic[k] = v * 2
+    return new_dic
+
+
+if __name__ == "__main__":
+    multiply_by_2(a_dictionary)
