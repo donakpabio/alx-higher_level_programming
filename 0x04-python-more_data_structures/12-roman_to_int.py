52b0137fed9a15f6aae0848ffcfa3f2975381e50
Namkere Akpabio <donakpabio@gmail.com>
2023-01-11 20:37:04 -0800

    t

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 
 
 def roman_to_int(roman_string) -> int:
-    if roman_string is not None or len(roman_string) > 0:
+    if roman_string is not None and type(roman_string) == str:
         rom = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
         v = 0
         for n in range(len(roman_string)):
