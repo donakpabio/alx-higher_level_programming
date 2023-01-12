f548b4c0e0fed0b30e43141084a86f1922248134
Namkere Akpabio <donakpabio@gmail.com>
2023-01-11 20:27:02 -0800

    t

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,14 +2,16 @@
 
 
 def roman_to_int(roman_string):
-    rom = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-    v = 0
-    for n in range(len(roman_string)):
-        if n > 0 and rom[roman_string[n]] > rom[roman_string[n - 1]]:
-            v = v + rom[roman_string[n]] - 2 * rom[roman_string[n - 1]]
-        else:
-            v = v + rom[roman_string[n]]
-    return v
+    if roman_string is not None or len(roman_string) > 0:
+        rom = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        v = 0
+        for n in range(len(roman_string)):
+            if n > 0 and rom[roman_string[n]] > rom[roman_string[n - 1]]:
+                v = v + rom[roman_string[n]] - 2 * rom[roman_string[n - 1]]
+            else:
+                v = v + rom[roman_string[n]]
+        return v
+    return None
 
 
 if __name__ == "__main__":
