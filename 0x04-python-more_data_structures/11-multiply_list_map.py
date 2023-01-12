c336b740fbb034db5352a55c42e0da2a66184590
Namkere Akpabio <donakpabio@gmail.com>
2023-01-11 20:01:08 -0800

    t

---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+def multiply_list_map(my_list=[], number=0):
+    return [x * number for x in my_list]
+
+
+if __name__ == "__main__":
+    multiply_list_map(my_list=[], number=0)
