c0fc0203fe296463c4164e6fda911714a4c43867
Namkere Akpabio <donakpabio@gmail.com>
2023-01-11 23:50:56 -0800

    t

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,12 +2,14 @@
 
 
 def weight_average(my_list=[]):
-    a = 0
-    b = 0
-    for n in range(len(my_list)):
-        a = a + my_list[n][0] * my_list[n][1]
-        b = b + my_list[n][1]
-    return a/b
+    if my_list is not None and len(my_list) > 0:
+        a = 0
+        b = 0
+        for n in range(len(my_list)):
+            a = a + my_list[n][0] * my_list[n][1]
+            b = b + my_list[n][1]
+        return a/b
+    return 0
 
 
 if __name__ == "__main__":
