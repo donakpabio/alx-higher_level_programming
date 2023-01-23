735747020fa98e0065174c87fb7057a7a6d69403
Namkere Akpabio <donakpabio@gmail.com>
2023-01-23 15:43:15 -0800

    t

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(int(value)))
+        return True
+    except ValueError:
+        return False
+
+
+if __name__ == "__main__":
+    safe_print_integer(value)
