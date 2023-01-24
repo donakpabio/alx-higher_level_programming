11d6cda776e126f2cc3ee5e5d80c909953bf3fc4
Namkere Akpabio <donakpabio@gmail.com>
2023-01-24 01:51:20 -0800

    t

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,14 +2,16 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    for n in range(x):
-        try:
-            print("{}".format(my_list[n]), end='')
-        except IndexError:
-            print("")
-            return n
-    print("")
-    return n + 1
+    if x > 0:
+        for n in range(x):
+            try:
+                print("{}".format(my_list[n]), end='')
+            except IndexError:
+                print("")
+                return n
+        print("")
+        return n + 1
+    return 0
 
 
 if __name__ == "__main__":
