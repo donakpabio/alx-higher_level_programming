8af9202800f203244677e08b03f90000375c1bad
Namkere Akpabio <donakpabio@gmail.com>
2023-01-25 14:08:20 -0800

    t

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,12 +2,11 @@
 
 
 def safe_print_division(a, b):
-    
     result = 0
     try:
         result = a / b
     except ZeroDivisionError:
-            result = None
+        result = None
     finally:
         print("Inside result: {}".format(result))
         return result
