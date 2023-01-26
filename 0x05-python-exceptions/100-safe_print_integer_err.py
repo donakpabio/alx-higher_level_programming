968690c08d912106e27a2623e778eacabff97a63
Namkere Akpabio <donakpabio@gmail.com>
2023-01-25 19:06:08 -0800

    t

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(int(value)))
+        return True
+    except (TypeError, ValueError) as err:
+        print("Exception: {}".format(err))
+
+
+if __name__ == "__main__":
+    list_division(my_list_1, my_list_2, list_length)
